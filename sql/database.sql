5cc5830f1497634e9ae1d913de7a529a5653cbf5
Luis Carlos Boch <luis.c.boch@gmail.com>
2014-05-25 13:35:05 -0300

    updated sql file

--- sql/database.sql
+++ sql/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE computer
+﻿CREATE TABLE computer
 (
   id serial NOT NULL,
   nome character varying(255) NOT NULL,
@@ -20,4 +20,84 @@ CREATE TABLE users
   creation_date timestamp(3) without time zone NOT NULL DEFAULT now(),
   last_update timestamp(3) without time zone NOT NULL DEFAULT now(),
   CONSTRAINT users_pkey PRIMARY KEY (id)
+);CREATE TABLE product_brand (
+    id serial primary key,
+    "name" character varying(255) NOT NULL,
+    creation_date timestamp(3) without time zone NOT NULL default now(),
+    description character varying(255),
+    last_update timestamp(3) without time zone NOT NULL default now(),
+    status boolean not null default true
 );
+
+
+CREATE TABLE shelf (
+    id serial primary key,
+    description character varying(255) NOT NULL,
+    code character varying(255) NOT NULL,
+    status boolean not null default true,
+    creation_date timestamp(3) not null default now(),
+    last_update timestamp(3) not null default now()
+);
+
+CREATE TABLE product (
+    id serial primary key,
+    "name" character varying(255) not null,
+    additional_code character varying(255),
+    barcode character varying(255) NOT NULL,
+    code character varying(255),
+    description character varying(255),
+    percentual_desc_max integer not null default 0,
+    min_quantity integer NOT NULL,
+    purchase_price double precision NOT NULL,
+    quantity integer NOT NULL,
+    sell_price double precision NOT NULL,
+    brand_id integer not null,
+    shelf_id integer NOT NULL,
+    status boolean not null default true,
+    creation_date timestamp(3) not null default now(),
+    last_update timestamp(3) not null default now(),
+    constraint fk_prdt_brand foreign key (brand_id)
+	references product_brand(id),
+    constraint fk_prdt_shelf foreign key(shelf_id)
+	references shelf(id)
+);
+
+
+CREATE TABLE product_keyword (
+    id serial primary key,
+    keywork character varying(255) NOT NULL,
+    product_id bigint NOT NULL,
+    constraint fk_keyw_prdt foreign key (product_id)
+	references product(id)
+);
+
+CREATE TABLE sale (
+    id serial primary key,
+    canceled boolean NOT NULL default false,
+    closed boolean NOT NULL default false,
+    close_date timestamp(3) NOT NULL default now(),
+    open_date timestamp(3) NOT NULL default now(),
+    total double precision default 0.0,
+    computer_id integer not null,
+    status boolean not null default true,
+    creation_date timestamp(3) not null default now(),
+    last_update timestamp(3) not null default now(),
+    constraint fk_sale_computer foreign key (computer_id)
+	references computer(id)
+);
+
+
+CREATE TABLE sale_product (
+    id serial primary key,
+    purchase_price double precision NOT NULL,
+    quantity integer NOT NULL,
+    sell_price double precision NOT NULL,
+    product_id bigint NOT NULL,
+    sale_id bigint NOT NULL,
+    status boolean not null default true,
+    creation_date timestamp(3) not null default now(),
+    last_update timestamp(3) not null default now(),
+    constraint fk_sale_prdt foreign key (product_id)
+	references product(id)
+);
+
